6f9c42380c4cbbc1810f2a4160fe2a5a97567fd9
Fritz <hoste.fritz@gmail.com>
2020-01-02 15:47:52 +0100

    changes to loading

--- src/neovis.js
+++ src/neovis.js
@@ -352,6 +352,8 @@ export default class NeoVis {
 					// );
 					this._network = new vis.Network(container, this._data, options);
 
+					document.getElementById("loadingBar").style.display = "block";
+
 					this._network.on("stabilizationProgress", function(params) {
 					    var maxWidth = 496;
 					    var minWidth = 20;
